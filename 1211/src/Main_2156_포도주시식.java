ab8f5d537b04d8dbd0cb0be14375d02813f58fe7
hj <tian1785@naver.com>
2019-12-17 17:52:18 +0900

    1217

---
+++ 1211/src/Main_2156_포도주시식.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main_2156_포도주시식 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int podo[] = new int[n];
+		for(int i=0; i<n; i++) {
+			podo[i] = sc.nextInt();
+		}
+		
+		int D[] = new int[n];
+		
+		
+		
+	}
+
+}
---
+++ 1211/src/Main_2225_합분해.java
@@ -0,0 +1,9 @@
+
+public class Main_2225_합분해 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

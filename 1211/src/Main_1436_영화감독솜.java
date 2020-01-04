524983f52b0e0bc139e4dd9eec8fbf35dfbe2977
hj <tian1785@naver.com>
2020-01-04 23:17:25 +0900

    0104

--- 1211/src/Main_1436_영화감독솜.java
+++ 1211/src/Main_1436_영화감독솜.java
@@ -1,9 +1,12 @@
+import java.util.Scanner;
 
 public class Main_1436_영화감독솜 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
 	}
 
 }

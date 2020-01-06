e554307929c13aa84de74fda4617ea8bbfd02906
hj <tian1785@naver.com>
2020-01-06 22:21:02 +0900

    0106

--- 1211/src/Main_2579_계단오르기.java
+++ 1211/src/Main_2579_계단오르기.java
@@ -1,6 +1,11 @@
-﻿
+﻿import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main_2579_계단오르기 {
 	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
+		int N = Integer.parseInt(br.readLine());
 	}
 }

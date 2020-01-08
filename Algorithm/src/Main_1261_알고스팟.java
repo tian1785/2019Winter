4d2776039e8a7952b59222225053d759db14c51a
tian1785 <tian1785@naver.com>
2020-01-08 17:45:18 +0900

    0108

---
+++ Algorithm/src/Main_1261_알고스팟.java
@@ -0,0 +1,9 @@
+
+public class Main_1261_알고스팟 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Algorithm/src/Main_2579_계단오르기.java
@@ -0,0 +1,23 @@
+﻿import java.util.Scanner;
+
+public class Main_2579_계단오르기 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] stairs = new int[N+1];
+		int[] D = new int[N+1];
+		
+		for(int i=1; i<=N; i++) {
+			stairs[i] = sc.nextInt();
+		}
+		
+		D[1] = stairs[1];
+		if(N>=2) D[2] = D[1] + stairs[2];
+		
+		for(int i=3; i<=N; i++) {
+			D[i] = Math.max(D[i-2] + stairs[i], D[i-3]+stairs[i]+stairs[i-1]);
+		}
+		
+		System.out.println(D[N]);
+	}
+}

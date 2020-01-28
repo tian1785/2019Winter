a762fff8f2e8d33fae6c6728bbbc0751a1182e54
tian1785 <tian1785@naver.com>
2020-01-28 21:40:39 +0900

    0128

--- Algorithm/src/Main_2225_합분해.java
+++ Algorithm/src/Main_2225_합분해.java
@@ -3,6 +3,7 @@ import java.io.InputStreamReader;
 
 public class Main_2225_합분해 {
 
+	static int N, M;
 	public static void main(String[] args) {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	}

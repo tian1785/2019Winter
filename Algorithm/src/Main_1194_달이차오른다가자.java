28bb1f6fe91024599cb23ecd4ccb8bd015e96a4b
tian1785 <tian1785@naver.com>
2020-01-28 21:39:49 +0900

    0128

--- Algorithm/src/Main_1194_달이차오른다가자.java
+++ Algorithm/src/Main_1194_달이차오른다가자.java
@@ -1,8 +1,9 @@
+import java.util.Scanner;
 
 public class Main_1194_달이차오른다가자 {
 
 	public static void main(String[] args) {
-
+		Scanner sc = new Scanner(System.in);
 	}
 
 }
--- Algorithm/src/Main_2225_합분해.java
+++ Algorithm/src/Main_2225_합분해.java
@@ -1,8 +1,10 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main_2225_합분해 {
 
 	public static void main(String[] args) {
-		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	}
 
 }

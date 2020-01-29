42ef6f880bd3c401358d8b599fcd4cabf1c768b1
최현정 <tian1785@naver.com>
2020-01-29 09:11:17 +0900

    0129:

--- Algorithm/src/Main_1194_달이차오른다가자.java
+++ Algorithm/src/Main_1194_달이차오른다가자.java
@@ -1,8 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main_1194_달이차오른다가자 {
 
-	public static void main(String[] args) {
-
+	static int M, N, Answer;
+	static char[][] map;
+	static boolean[][] visit;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		map = new char[N][M];
+		
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<M; j++) {
+				map[i] = br.readLine().toCharArray();
+			}
+		}
 	}
 
 }
---
+++ Algorithm/src/Solution_D4_1249_보급로_DFS.java
@@ -0,0 +1,9 @@
+
+public class Solution_D4_1249_보급로_DFS {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
